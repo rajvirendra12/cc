666195bebe2cfb31f2f29e159ac979ea258059a1
rajvirendra12 <140872979+rajvirendra12@users.noreply.github.com>
2023-07-29 20:00:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canvas, backgroundImage;
+
+var questions;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  quiz = new Quiz();
+//call the start() method inside the quiz class
+  quiz.start()
+}
+
+
+function draw(){
+  background("pink");
+
+}
